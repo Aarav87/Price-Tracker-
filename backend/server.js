60a27f5ece5e97abfce0c810fd14a621aa09ca22
Aarav <43528744+Aarav87@users.noreply.github.com>
2021-01-15 13:15:28 -0500

    updated serviceAccount

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,13 @@ const nodemailer = require('nodemailer')
 const cheerio = require('cheerio')
 const admin = require('firebase-admin')
 const axios = require('axios')
-const serviceAccount = require('./ServiceAccountKey')
-require('dotenv').config()
 
 const app = express();
 
 //use cors to allow cross origin resource sharing
 app.use(
   cors({
-    origin: 'https://www.amazon.ca',
+    origin: 'https://price-tracker-amz.herokuapp.com',
     credentials: true,
   })
 );
@@ -22,6 +20,19 @@ app.use(
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+const serviceAccount = {
+    "type": process.env.TYPE,
+    "project_id": process.env.PROJECT_ID,
+    "project_key_id": process.env.PROJECT_KEY_ID,
+    "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.CLIENT_EMAIL,
+    "client_id": process.env.CLIENT_ID,
+    "auth_uri": process.env.AUTH_URI,
+    "token_uri": process.env.TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER,
+    "client_x509_cert_url": process.env.CENT_URL
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
