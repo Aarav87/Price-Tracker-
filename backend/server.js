60bea48bcc2b3d2d6744af252bcf1de19ee63dfd
Aarav <43528744+Aarav87@users.noreply.github.com>
2021-02-08 13:13:46 -0500

    updated checkPrice function

--- backend/server.js
+++ backend/server.js
@@ -172,6 +172,38 @@ app.post('/deleteProduct', function(req, res) {
         
 })
 
+async function updateProductDetails(url, item) {
+    const productDetails = await getProductDetails(url)
+
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    const today = new Date();
+    const dd = String(today.getDate())
+    const mm = String(today.getMonth())
+    const date = `${months[mm]} ${dd}`
+
+    const priceHistory = item.priceHistory
+    const dateRecorded = item.dateRecorded  
+
+    if(dateRecorded[dateRecorded.length - 1] != date) {
+        priceHistory.push(productDetails.currentPrice)
+        dateRecorded.push(date)
+        
+        setTimeout(() => {
+            var ref = db.collection('users').doc(user.email).collection('products').doc(item.productTitle)
+            ref.update({
+                productTitle: productDetails.productTitle,
+                currentProductPrice: productDetails.currentPrice,
+                imageUrl: productDetails.imageUrl,
+                priceHistory: priceHistory,
+                dateRecorded: dateRecorded,
+                youSave: productDetails.youSave 
+            }).catch(error => {
+                console.log(error)
+            })
+        }, 3000)
+    }       
+}
+
 function checkPrice() {
     if(user) {
         db.collection(`/users/${user.email}/products`)
@@ -183,83 +215,12 @@ function checkPrice() {
                     items.push(data);
                 });
 
-                const productDetails = async items => {
-                    if(Array.isArray(items) || !items === null) {
-                        console.log(items)
-                        const browser = await puppeteer.launch({headless: true, args: ["--no-sandbox"]})
-                        
-                        items.forEach(async item => {
-                            const url = item.url
-                            var productDetails = {}  
-
-                            try {
-                                const page = await browser.newPage()
-                                await page.goto(url)
-
-                                const getProductDetails = await page.evaluate(() => {
-                                    const productTitle = document.querySelector('#productTitle').innerText;
-                                    var currentPrice = document.querySelector('#priceblock_ourprice').innerText;
-                                    const imageUrl = document.querySelector('#landingImage').src
-                                    var youSave = document.querySelector('#regularprice_savings')
-                                    
-                                    if(currentPrice === "") {
-                                        currentPrice = document.body.querySelector('#priceblock_dealprice').innerText
-                                    } 
-
-                                    if(youSave != null) {
-                                        youSave = youSave.innerText
-                                    } else {
-                                        youSave = ""
-                                    }
-
-                                    var data = {
-                                        productTitle, 
-                                        currentPrice,
-                                        imageUrl,
-                                        youSave
-                                    }
-
-                                    return data   
-                                })
-
-                                productDetails = getProductDetails
-                            } 
-                            catch(e) {
-                                console.log(e)
-                            }
-
-                            const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-                            const today = new Date();
-                            const dd = String(today.getDate())
-                            const mm = String(today.getMonth())
-                            const date = `${months[mm]} ${dd}`
-
-                            const priceHistory = item.priceHistory
-                            const dateRecorded = item.dateRecorded  
-
-                            if(dateRecorded[dateRecorded.length - 1] != date) {
-                                priceHistory.push(productDetails.currentPrice)
-                                dateRecorded.push(date)
-                                
-                                var ref = db.collection('users').doc(user.email).collection('products').doc(item.productTitle)
-                                ref.update({
-                                    productTitle: productDetails.productTitle,
-                                    currentProductPrice: productDetails.currentPrice,
-                                    imageUrl: productDetails.imageUrl,
-                                    priceHistory: priceHistory,
-                                    dateRecorded: dateRecorded,
-                                    youSave: productDetails.youSave 
-                                }).catch(error => {
-                                    console.log(error)
-                                })
-                            }       
-                        })
-
-                        browser.close()
-                    } 
-                }
-
-                productDetails(items)
+                if(Array.isArray(items) || !items === null) {
+                    items.forEach(item => {
+                        const url = item.url
+                        updateProductDetails(url, item)
+                    })
+                } 
             })
     }
 }
