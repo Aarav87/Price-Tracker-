8774f17d502c0bc9f5b91e972e9a1bdcafe5c708
Aarav <43528744+Aarav87@users.noreply.github.com>
2021-02-13 14:56:28 -0500

    updated backend

--- backend/server.js
+++ backend/server.js
@@ -202,24 +202,26 @@ async function checkPrice() {
     const listUsers = await admin.auth().listUsers()
         
     Object.values(listUsers)[0].forEach((user, index) => {
-        const email = user.toJSON()['email']
-        db.collection(`/users/${email}/products`)
-            .get()
-            .then(snapshot => {
-                const items = [];
-                snapshot.forEach(document => {
-                    const data = document.data();
-                    items.push(data);
-                });
+        setTimeout(() => {
+            const email = user.toJSON()['email']
+            db.collection(`/users/${email}/products`)
+                .get()
+                .then(snapshot => {
+                    const items = [];
+                    snapshot.forEach(document => {
+                        const data = document.data();
+                        items.push(data);
+                    });
 
-                if(Array.isArray(items) || !items === null) {
-                    items.forEach((item, index) => {
-                        setTimeout(() => {
-                            updateProductDetails(item, email)
-                        }, index * 5000)
-                    })
-                } 
-            })
+                    if(Array.isArray(items) || !items === null) {
+                        items.forEach((item, index) => {
+                            setTimeout(() => {
+                                updateProductDetails(item, email)
+                            }, index * 5000)
+                        })
+                    } 
+                })
+        }, index * 10000)
     })
 }
 
