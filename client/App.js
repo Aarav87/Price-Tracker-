672d9df843a6c15881805f7c3aaafadcbe12b046
Aarav <43528744+Aarav87@users.noreply.github.com>
2021-03-02 16:10:16 -0500

    updated app

--- client/App.js
+++ client/App.js
@@ -154,8 +154,8 @@ class App extends Component {
     })
 
     setTimeout(this.getProductDetails, 2000)
-    setTimeout(this.addProduct, 9000)
-    setTimeout(this.updateList, 12000)
+    setTimeout(this.addProduct, 12000)
+    setTimeout(this.updateList, 15000)
 
   }
   
