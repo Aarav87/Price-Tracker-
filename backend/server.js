98297ac3d962f5bacd4c0dcbd1c2d56b98e4aec3
Aarav <43528744+Aarav87@users.noreply.github.com>
2021-03-07 17:56:21 -0500

    updated backend

--- backend/server.js
+++ backend/server.js
@@ -278,7 +278,7 @@ async function priceMet() {
     })
 }
 
-setInterval(checkPrice, 36000000)
-setInterval(priceMet, 43200000)
+setInterval(checkPrice, 21600000)
+setInterval(priceMet, 28800000)
 
 app.listen(PORT);
